f764b0e8b4abf483cc20a914d1bb64d65e0d2449
Loren Stone <97418895+AncientLor@users.noreply.github.com>
2023-02-28 20:11:46 -0800

    Updated Install.sh

--- filebeat/install.sh
+++ filebeat/install.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-docker run -d -it --name=filebeat --hostname=cheese.beat --user=root --network=filebeat -p 8080:8080 -v /var/log:/usr/share/var/log:ro -v ~/.elk/filebeat/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /var/run/docker.sock:/var/run/docker.sock:ro docker.elastic.co/beats/filebeat:8.6.2 filebeat -e --strict.perms=false
+docker network create filebeat
+docker run -d -it --name=filebeat --hostname=cheese.beat --user=root --network=filebeat -v /var/log:/usr/share/var/log:ro -v ~/.elk/filebeat/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /var/run/docker.sock:/var/run/docker.sock:ro docker.elastic.co/beats/filebeat:8.6.2 filebeat -e --strict.perms=false
