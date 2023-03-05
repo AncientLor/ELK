00ba9ebc975dc37df416e959f0543658e8bdb883
Loren Stone <97418895+AncientLor@users.noreply.github.com>
2023-03-04 16:26:41 -0800

    Added filebeat install files

--- filebeat/install.sh
+++ filebeat/install.sh
@@ -1,3 +1,45 @@
 #! /bin/bash
-docker network create filebeat
-docker run -d -it --name=filebeat --hostname=host-beat --user=root --network=filebeat -v /var/log:/usr/share/var/log:ro -v ~/.elk/filebeat/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /var/run/docker.sock:/var/run/docker.sock:ro docker.elastic.co/beats/filebeat:8.6.2 filebeat -e --strict.perms=false
+
+# Check if user has sudo permissions
+
+if [[ $(id -u) -ne 0 ]]; then
+  echo -e "\e[1;36m This script must be run with sudo privileges. Please use 'sudo' to run this script. \e[0m"
+  exit
+fi
+
+# Check if elkserver host exists
+
+ELKSERVER=$(getent hosts | grep elkserver);
+
+if [ x${ELKSERVER} == x ]; then
+    echo "Add elkserver to /etc/hosts";
+    exit 1;
+fi
+
+FILE=~/.elk/certs/ca.crt
+
+if [ -f "$FILE" ]; then
+    echo "Certs found... Proceeding.";
+else
+    echo "$FILE not found. Please add certs to ~/.elk/certs";
+    exit 1;
+fi
+
+apt update && apt install filebeat;
+
+sed -e 's/output.elasticsearch:/#output.elasticsearch:/; s/hosts: \["localhost:9200"\]/#hosts: \["localhost:9200"\]/; s/#output.logstash:/output.logstash:/; s/#hosts: \["localhost:5044"\]/hosts: \["elkserver:5044"\]/;' /etc/filebeat/filebeat.yml;
+
+filebeat modules enable system && filebeat modules list;
+
+filebeat setup --pipelines --modules system;
+
+filebeat setup --index-management -E output.logstash.enabled=false -E 'output.elasticsearch.hosts=["https://elkserver:9200"]' -E 'output.elasticsearch.ssl.certificate_authorities: ["~/.elk/certs/ca.pem"]' -E 'output.elasticsearch.ssl.certificate: "~/.elk/certs/elasticsearch.pem"' -E 'output.elasticsearch.ssl.key: "~/.elk/certs/elasticsearch.key"'
+
+filebeat setup -E output.logstash.enabled=false -E 'output.elasticsearch.hosts=["https://elkserver:9200"]' -E 'output.elasticsearch.ssl.certificate_authorities: ["~/.elk/certs/ca.pem"]' -E 'output.elasticsearch.ssl.certificate: "~/.elk/certs/elasticsearch.pem"' -E 'output.elasticsearch.ssl.key: "~/.elk/certs/elasticsearch.key"' -E 'setup.kibana.host=elkserver:5601';
+
+systemctl start filebeat && systemctl enable filebeat;
+
+curl -XGET 'https://elkserver:9200/filebeat-*/_search?pretty';
+
+exit;
+
